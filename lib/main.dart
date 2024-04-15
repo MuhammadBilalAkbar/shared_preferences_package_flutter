8b648e584c927ae158729e0d5279de1e2503c79c
MuhammadBilalAkbar <mbilalakbarkh786@gmail.com>
2024-04-16 00:05:41 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import '../check_status.dart';
+import 'utils/check_status.dart';
 import '../theme/theme.dart';
 import 'firebase_options.dart';
 
